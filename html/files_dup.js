c54f70fba78e332fde0f8ce1a5bfee6979aae89d
vmali12345 <vitthal.mali@linkedloops.com>
2025-03-14 16:57:01 +0530

    Add files via upload

---
+++ html/files_dup.js
@@ -0,0 +1,4 @@
+var files_dup =
+[
+    [ "Workspace", "dir_f243a7a7d497212ea32570f02ce73440.html", "dir_f243a7a7d497212ea32570f02ce73440" ]
+];
---
+++ html/gapm__task_8c.js
@@ -0,0 +1,18 @@
+var gapm__task_8c =
+[
+    [ "gapm_set_address_type", "gapm__task_8c.html#ab354fcc1a4086aca796f3ed534a3f297", null ],
+    [ "gapm_addr_solved_ind_handler", "gapm__task_8c.html#ae24a507dc252fd5d6f9658ff3cbddb61", null ],
+    [ "gapm_adv_report_evt_handler", "gapm__task_8c.html#ad0792e97ff966fcc9979e30fc79ec481", null ],
+    [ "gapm_cancel_cmd_handler", "gapm__task_8c.html#abdfb6d62a7d9ae7998f4391e429e55a6", null ],
+    [ "gapm_cmp_evt", "gapm__task_8c.html#a560d5876d76afb74aafc1ad4ce801eb7", null ],
+    [ "gapm_connection_cfm_handler", "gapm__task_8c.html#ac0ea2e6ec23b5d1c1cf3d42c82421f68", null ],
+    [ "gapm_dev_bdaddr_ind_handler", "gapm__task_8c.html#a2b18ffd85480dcb6524dbdbd618fcd54", null ],
+    [ "gapm_reset_req_handler", "gapm__task_8c.html#a5f9336bc838881f7c6ffe7bd70caf201", null ],
+    [ "gapm_resolv_addr_cmd_handler", "gapm__task_8c.html#af46cb06ec161da52185e23d8252daa3e", null ],
+    [ "gapm_set_dev_config_cmd_handler", "gapm__task_8c.html#a1c348b7be331c6be7576955eff729d6d", null ],
+    [ "gapm_set_dev_name_handler", "gapm__task_8c.html#ac9a36005511f788d55637f1dc287a820", null ],
+    [ "gapm_start_adv_cmd_handler", "gapm__task_8c.html#ac1beaca1f3d17c8e681cce65419c33b0", null ],
+    [ "gapm_start_connection_cmd_handler", "gapm__task_8c.html#a91201300a1e074b251820382033d741e", null ],
+    [ "gapm_start_scan_cmd_handler", "gapm__task_8c.html#aba3fd4f2182515706d8b169368d7d99f", null ],
+    [ "gapm_white_list_mgm_cmd", "gapm__task_8c.html#a6d31cafe46666b2df039035093d493e7", null ]
+];
